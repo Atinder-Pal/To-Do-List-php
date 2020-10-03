f0d8f733d4623066b425db46cc1f753c828cad82
Atinder-Pal <atty.pal46@gmail.com>
2020-10-03 15:16:57 -0600

    added functionality to store new task in Session and display it in  list vis session variable

--- addToDo.php
+++ addToDo.php
@@ -1,18 +1,28 @@
 <?php
     session_start();
-
+    //check if $_SESSION['task'] has been set or not, if not-set it to an array
     if ( !isset( $_SESSION['task'] ) )
     {
         $_SESSION['task'] = array();
     }
-
-    if( !empty($_POST) )
+    //check if their a submission via "POST" and the $_POST['newTask'] is not submitted empty
+    if( !empty($_POST) && $_POST['newTask'] != '' )
     {
         array_push($_SESSION['task'], $_POST['newTask'] );
     }
     
 ?>
 <h3>To-Do List</h3>
-<?php 
-    var_dump($_SESSION['task']);
-?>
+
+<!--For TESTING: var_dump($_SESSION['task']); -->
+<?php if ( isset($_SESSION['task']) ) : ?>
+    <ul>
+        <?php foreach ( $_SESSION['task'] as $newTask ) : ?>
+            <li>
+                <?php echo $newTask; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+   
+      
--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include dirname(__FILE__).'/templates/header.php';
 // Show Form to submit new task to List
 include dirname(__FILE__).'/templates/form.php';
 
+//Show To-Do List and add tasks to it as we submit new tasks via form
 include dirname(__FILE__).'/addToDo.php';
 
 // Show Footer
