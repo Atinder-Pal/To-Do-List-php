b58d55c50fb5a8ccba455b5a77ecda6d6138ed17
Atinder-Pal <atty.pal46@gmail.com>
2020-10-03 12:30:24 -0600

    added footer.php

---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <h2>Footer Details</h2>
+    <p>
+      &copy;
+      <!-- Citation -->
+      <!-- Refernced from Warren's Repo for Classroom Practice -->
+      <?php echo date( 'Y' ); // Output current year (based on server computer time.) ?>
+      <!-- End Citation -->
+      Atinder Pal
+    </p>
+  </footer>
+</body>
+</html>
