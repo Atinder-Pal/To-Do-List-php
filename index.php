52ec57c1313fdf7f47dfaff37b4501b5d2bfbe4c
Atinder-Pal <atty.pal46@gmail.com>
2020-10-03 12:42:07 -0600

    added form.php template to add new task

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 // Show Header
 include dirname(__FILE__).'/templates/header.php';
 
+// Show Form to submit new task to List
+include dirname(__FILE__).'/templates/form.php';
+
 // Show Footer
 include dirname(__FILE__).'/templates/footer.php';
