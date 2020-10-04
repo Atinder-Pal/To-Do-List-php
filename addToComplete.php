47894b70025f5cca2604d769728a7b38bcb28f58
Atinder-Pal <atty.pal46@gmail.com>
2020-10-03 22:55:00 -0600

    displayed checked tasks from To-Do in the Completed To-Dos

--- addToComplete.php
+++ addToComplete.php
@@ -7,11 +7,7 @@
     if( isset($_POST['complete']) )
     {
         if( !empty($_POST['completedTasks']) )
-        {
-            // foreach($_POST['completedTasks'] as $completedTask)
-            // {
-            //     echo $completedTask."</br>";
-            // }
+        {            
             //Thanks to Birm, Fahad and Ummer for explaining concept of '...' operator
             $_SESSION['completedList'] = [...$_SESSION['completedList'],...$_POST['completedTasks']];            
         }
@@ -22,10 +18,8 @@
 <h3>Completed To-Dos</h3>
 <?php if ( isset($_SESSION['completedList']) ) : ?>
     <ul>
-        <?php foreach ( $_SESSION['task'] as $newTask ) : ?>
-            <li>
-            <input type="checkbox" name="completedTasks[]" value='<?php echo $newTask; ?>'><?php echo $newTask; ?></input>
-            </li>
+        <?php foreach ( $_SESSION['completedList'] as $completedTask ) : ?>
+            <li> <?php echo $completedTask; ?> </li>
         <?php endforeach; ?>
     </ul>
     <?php endif; ?>
